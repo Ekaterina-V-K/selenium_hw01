bf7194b494c399c09f52a5f914513cd468404aa4
Ekaterina-V-K <kuznetsova.ekaterina.v@gmail.com>
2024-04-21 12:41:50 +0300

    Задание 3. Сделайте сценарий логина
    Сделайте сценарий для логина в панель администрирования учебного приложения http://localhost/litecart/admin/.
    
    Проверки можно пока никакие не делать, только действия -- заполнение полей, нажатия на кнопки и ссылки.
    
    Можно оформить сценарий либо как тест, либо как отдельный исполняемый файл.
    
    Не забывайте о том, что браузер должен останавливаться, даже если возникли какие-либо ошибки во время выполнения сценария.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 @pytest.fixture
 def driver(request):
     wd = webdriver.Chrome()
@@ -11,11 +10,10 @@ def driver(request):
     request.addfinalizer(wd.quit)
     return wd
 
-
 def test_example(driver):
-    driver.get("http://www.google.com/")
-    #driver.find_element("name","q").send_keys("webdriver")
-    #driver.find_element("name","btnK").click()
-
-    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Поиск в Google"))
+    driver.get("http://localhost/litecart/admin/")
+    driver.find_element("name","username").send_keys("admin")
+    driver.find_element("name","password").send_keys("admin")
+    driver.find_element("name","login").click()
+    #WebDriverWait(driver, 10).until(EC.title_is("webdriver - Поиск в Google"))
     driver.quit()
---
+++ test_20240421-ex3.py
@@ -0,0 +1,36 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    print(wd.capabilities)
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    driver.get("http://localhost/litecart/admin/")
+    driver.find_element("name", "username").send_keys("admin")
+    driver.find_element("name", "password").send_keys("admin")
+    driver.find_element("name", "login").click()
+
+    # search_box = driver.find_element("name", "q")
+    # search_box.send_keys('ChromeDriver')
+    # search_box.submit()
+
+
+# WebDriverWait(driver, 10).until(EC.title_is("webdriver - Поиск в Google"))
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
